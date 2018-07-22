99c327f18c8e021d9176afc8499e140fc3707fd3
unitbii <unitbii@unitbiideMacBook-Pro.local>
2018-07-22 23:19:28 +0800

    在菜单系统中融入了自定义的组件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,37 @@
 <style lang="less">
-  .layout-con {
-    height: 100%;
-    width: 100%;
-  }
-  .menu-item span {
+.menu-item {
+  span {
     display: inline-block;
     overflow: hidden;
     width: 69px;
-    text-overflow: ellipsis;
+    // text-overflow: ellipsis;
     white-space: nowrap;
     vertical-align: bottom;
-    transition: width .2s ease .2s;
+    transition: width 0.2s ease 0.2s;
   }
-  .menu-item i {
+  i {
     transform: translateX(0px);
     transition: font-size .2s ease, transform .2s ease;
     vertical-align: middle;
     font-size: 16px;
   }
-  .collapsed-menu span {
+}
+.collapsed-menu {
+  span {
     width: 0px;
-    transition: width .2s ease;
+    transition: width 0s;
   }
-  .collapsed-menu i {
+  i {
     transform: translateX(5px);
     transition: font-size .2s ease .2s, transform .2s ease .2s;
-    vertical-align: middle;
     font-size: 22px;
   }
+}
 </style>
 
 <template>
   <div class="layout">
-    <i-layout :style="{minHeight: '100vh'}">
+    <i-layout>
       <i-sider collapsible :collapsed-width="78" v-model="isCollapsed">
         <left-sider :isCollapsed="isCollapsed"></left-sider>
       </i-sider>
@@ -58,7 +57,7 @@ import LeftSider from '@/components/LeftSider'
 export default {
   data () {
     return {
-      isCollapsed: false
+      isCollapsed: true
     }
   },
   components: { LeftSider }
--- src/components/LeftSider.vue
+++ src/components/LeftSider.vue
@@ -1,22 +1,39 @@
 <style lang="less" scoped>
-  .user-information {
-    padding: 20px 0;
+  .user-infoPlate {
+    cursor: pointer;
+    padding: 14px 0;
     text-align: center;
-    .name {
-      color: #fff;
-      display: inline-block;
-      font-size: 16px;
-      padding-top: 8px;
+    div {
+      height: 64px;
+      transition: all 0.2s;
+      transform-origin: center top;
+      .name {
+        color: #fff;
+        font-size: 16px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        transition: opacity 0.2s;
+      }
+    }
+    div.collapsed-user {
+      height: 24px;
+      transform: scale(0.6);
+      .name {
+        opacity: 0;
+      }
     }
   }
 </style>
 
 <template>
-  <div>
+  <div class="leftBar">
     <!-- 用户信息 -->
-    <div class="user-information">
-      <span><i-avatar icon="person" size="large"/></span><br/>
-      <span class="name">unitbii</span>
+    <div class="user-infoPlate">
+      <div :class="{'collapsed-user' : this.isCollapsed }">
+        <i-avatar icon="person" size="large"/>
+        <p class="name">{{ userDate.name }}</p>
+      </div>
     </div>
     <!-- 菜单栏 -->
     <i-menu :active-name="menuData.activeName" :theme="menuData.theme" width="auto" :class="menuitemClasses">
@@ -34,7 +51,7 @@ export default {
     return {
       menuData: {
         theme: 'dark',
-        activeName: '1-1',
+        activeName: '1-2',
         items: {
           '1-1': {
             name: '1-1',
@@ -52,6 +69,9 @@ export default {
             title: '设置'
           }
         }
+      },
+      userDate: {
+        name: 'unitbii'
       }
     }
   },
